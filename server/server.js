f680d9fbc6295c861f90c56c37063924651e06ed
Samrat De <desamrat31@gmail.com>
2021-03-17 02:35:00 +0530

    changed server.js for production

--- server/server.js
+++ server/server.js
@@ -9,13 +9,22 @@ const app = express()
 
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to expense tracker API' })
-})
-
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/expenses', require('./routes/expenses'))
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.json({ msg: 'Welcome to expense tracker API' })
+  })
+}
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
